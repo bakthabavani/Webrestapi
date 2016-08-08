e9c16d08f9ef03dac4a6be1a585df05e22dc6cd3
admin <vamsikrishna@vamsikrishna-Vostro-3558>
2016-08-08 09:56:47 +0530

    removed to add all new

--- sample_data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO S_USERS(username,date_of_birth) VALUES('Vamsi','1990-10-08');
-INSERT INTO S_USERS(username,date_of_birth) VALUES('Kalyan','1992-10-08');
-INSERT INTO S_USERS(username,date_of_birth) VALUES('Krishna','1990-10-08');
--- src/main/java/com/s/dao/GenreDAO.java
+++
@@ -1,7 +0,0 @@
-package com.s.dao;
-
-import com.s.entities.Genre;
-
-public interface GenreDAO {
-	public Genre getGenre(int genreID);
-}
